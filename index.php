48863f7dc0408f31423f512cb78831b4ea1250ef
LeonardoRoncelli <160123628+LeonardoRoncelli@users.noreply.github.com>
2026-05-05 10:54:04 +0200

    Add files via upload
    Iscrizione

--- index.php
+++ index.php
@@ -24,5 +24,25 @@ if(isset($_POST['azione']) && $_POST['azione'] == 'login'){
 if(isset($_POST['aggiungi_iscritto'])){
     $id_membro=$POST['membro'];
     $id_corso=$_POST['corso'];
-    
+    $conn->query("INSERT INTO iscrizioni_corsi(membro,corso) VALUES ('$id_membro', $id_corso")
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Palestra</title>
+</head>
+<body>
+    <h2>Aggiungi iscritto</h2>
+    <form method="POST">
+        <?php
+        $id_membro:<input type="text" name="membro" required>
+        $id_corso:$conn-> query("SELECT * from membri");
+        while($a=$membri->fetch_assoc()){
+            echo <option value="'$a['id']}>($a['id_membro')}"</option>
+        }
+    ?>
+</body>
+</html>
